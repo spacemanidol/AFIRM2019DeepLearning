59f6c22d1e04e8a18bb40456797527ad5de34b24
Your Name <you@example.com>
2019-01-16 23:20:23 -0800

    updates

--- README.md
+++ README.md
@@ -1,5 +1,8 @@
 # AFRIRM Deep learning Track
-
+## Overview
+On January 12 2019 Bhaskar Mitra, Nick Craswell, Emine Yilmaz, and Daniel Campos gave lectures and labs about Deep Learning for Information Retrieval and Data Mining at [AFIRM 2019](http://sigir.org/afirm2019/) by SIGIR. This repo contains the lab portion of the deep dive. 
+### Setup
+We suggest you use conda to deal with specific enviorments but feel free to ignore.
 ```bash
 conda create -n afirm python=3.6
 source activate afirm
@@ -10,4 +13,8 @@ pip install -U pandas
 pip install -U numpy
 pip install matplotlib
 pip install -U tqdm
+./prep.sh
 ```
+The prep.sh file will donwload the relevant data.
+
+
--- prep.sh
+++ prep.sh
@@ -1,12 +1,15 @@
+cd data
 wget https://msmarco.blob.core.windows.net/msmarcoranking/collection.tar.gz
 wget https://msmarco.blob.core.windows.net/msmarcoranking/queries.tar.gz
+wget https://msmarco.blob.core.windows.net/msmarcoranking/letor.tar.gz
+wget https://msmarco.blob.core.windows.net/msmarcoranking/models.tar.gz
 wget http://nlp.stanford.edu/data/glove.6B.zip
-mkdir data
-mv glove.6B.zip data
-unzip data/glove.6b.zip
+wget http://nlp.stanford.edu/data/glove.twitter.27B.zip
+unzip glove.6b.zip
+unzip glove.twitter.27B.zip
 tar -xzvf queries.tar.gz
 tar -xzvf collection.tar.gz
-mv queries.*.tsv data/
-mv collection.tsv data/
+tar -xzvf letor.tar.gz
+tar -xzvf models.tar.gz
 rm *.tar.gz
 rm *.zip
