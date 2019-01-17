57e32b402ce2486ae38f974c83b77c7079676c33
spacemanidol <dacamp@microsoft.com>
2019-01-17 07:45:24 -0800

    Update prep.sh

--- prep.sh
+++ prep.sh
@@ -5,8 +5,8 @@ wget http://nlp.stanford.edu/data/glove.6B.zip
 unzip glove.6B.zip
 wget http://nlp.stanford.edu/data/glove.twitter.27B.zip
 unzip glove.twitter.27B.zip
-#wget https://msmarco.blob.core.windows.net/msmarcoranking/collection.tar.gz
-#tar -xzvf collection.tar.gz
+wget https://msmarco.blob.core.windows.net/msmarcoranking/collection.tar.gz
+tar -xzvf collection.tar.gz
 wget https://msmarco.blob.core.windows.net/msmarcoranking/letor.tar.gz
 tar -xzvf letor.tar.gz
 wget https://msmarco.blob.core.windows.net/msmarcoranking/models.tar.gz
