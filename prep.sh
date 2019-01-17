075f7a15ca0f016a8da175e865afa46a7dd988b7
Your Name <you@example.com>
2019-01-17 03:22:46 -0800

    prep.sh

--- prep.sh
+++ prep.sh
@@ -1,15 +1,15 @@
 cd data
-wget https://msmarco.blob.core.windows.net/msmarcoranking/collection.tar.gz
 wget https://msmarco.blob.core.windows.net/msmarcoranking/queries.tar.gz
-wget https://msmarco.blob.core.windows.net/msmarcoranking/letor.tar.gz
-wget https://msmarco.blob.core.windows.net/msmarcoranking/models.tar.gz
-wget http://nlp.stanford.edu/data/glove.6B.zip
-wget http://nlp.stanford.edu/data/glove.twitter.27B.zip
-unzip glove.6b.zip
-unzip glove.twitter.27B.zip
 tar -xzvf queries.tar.gz
-tar -xzvf collection.tar.gz
+wget http://nlp.stanford.edu/data/glove.6B.zip
+unzip glove.6B.zip
+wget http://nlp.stanford.edu/data/glove.twitter.27B.zip
+unzip glove.twitter.27B.zip
+#wget https://msmarco.blob.core.windows.net/msmarcoranking/collection.tar.gz
+#tar -xzvf collection.tar.gz
+wget https://msmarco.blob.core.windows.net/msmarcoranking/letor.tar.gz
 tar -xzvf letor.tar.gz
+wget https://msmarco.blob.core.windows.net/msmarcoranking/models.tar.gz
 tar -xzvf models.tar.gz
 rm *.tar.gz
 rm *.zip
